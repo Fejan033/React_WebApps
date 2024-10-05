c11fec5d7147a22937ff385500300f82cebdb499
Mohammed <fejan033@gmail.com>
2024-10-05 13:49:43 +0530

    expense_css_added

--- reactwebapps/src/ExpenseTracker/Expense.jsx
+++ reactwebapps/src/ExpenseTracker/Expense.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-import { useState } from 'react';
-import './Expense.css'; 
+import React, { useState } from "react";
+import "./Expense.css";
 
 const Expense = () => {
-  const[amount, setAmount] = useState(0)
+  const [expensetype, setExpensetype] = useState('');
+  const [amount, setAmount] = useState('');
+  const [expenses, setExpenses] = useState([]); 
+
+  const handleType = (event) => {
+    setExpensetype(event.target.value);
+  };
 
   const handleAmount = (event) => {
     setAmount(event.target.value);
@@ -11,23 +16,57 @@ const Expense = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(amount);
-  }
+    
 
+    if (!expensetype || !amount) return;
+
+    
+    setExpenses([...expenses, { type: expensetype, amount: amount }]);
+
+    
+    setExpensetype('');
+    setAmount('');
+  };
 
   return (
-    <div className="container">
-      <div className="header">
-        <h1>Your Expenses</h1>
-      </div>
-      <div>
-        <input type="text" placeholder="Expense Type" />
-        <input type="number" placeholder="Enter Amount" onChange={handleAmount}/>
-        <button className="Button" onClick={handleSubmit}>Add Expense</button>
-      </div>
-      <div>
-      <h1>Your Expenses:</h1>
-      
+    <div className="maincontainer">
+      <div className="container">
+        <div className="header">
+          <h1>Your Expenses</h1>
+        </div>
+        <div className="expensebar">
+
+          <input
+            type="text"
+            value={expensetype}
+            placeholder="Expense Type"
+            onChange={handleType}
+            className="input1"
+          />
+
+          <input
+            type="number"
+            value={amount}
+            placeholder="Enter Amount"
+            onChange={handleAmount}
+            className="input1"
+          />
+
+          <button className="Button" onClick={handleSubmit}>
+            Add Expense
+          </button>
+        </div>
+
+        <div>
+          <h2>Your Expenses:</h2>
+          <ul>
+            {expenses.map((expense, index) => (
+              <li key={index}>
+                Expense Type: {expense.type} | Expense Amount: ${expense.amount}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
