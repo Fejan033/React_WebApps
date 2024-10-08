eb5d05978db2f01ffeb8a683723b6cde335b532f
Mohammed <fejan033@gmail.com>
2024-10-08 22:51:29 +0530

    ToDo_Added_deletebutton

--- reactwebapps/src/ToDo/Todo.jsx
+++ reactwebapps/src/ToDo/Todo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./ToDo.css"; 
+import "./ToDo.css";
 
 const Todo = () => {
   const [inputvalue, setinputvalue] = useState("");
@@ -17,6 +17,12 @@ const Todo = () => {
     }
   };
 
+  const deletetodo = (id) => {
+    const updatedtodos = todos.filter((todo) => todo.id !== id);
+    settodos(updatedtodos);
+  };
+  
+
   return (
     <div className="container">
       <h1>Your Daily Task</h1>
@@ -26,12 +32,15 @@ const Todo = () => {
         value={inputvalue}
         onChange={(e) => setinputvalue(e.target.value)}
       />
-      <button type="submit" onClick={addtodo}>
+      <button className='addbutton' type="submit" onClick={addtodo}>
         Add Task
       </button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => deletetodo(todo.id)}>Delete Task</button>
+          </li>
         ))}
       </ul>
     </div>
